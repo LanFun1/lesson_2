6d06b2ba9cef54aea9ab7aa4938b507365a94df9
Daniel <vik-vay.av@yandex.ru>
2021-05-28 21:47:49 +0300

    Основная часть

---
+++ script.js
@@ -0,0 +1,18 @@
+let money = 123000;
+let income = "freelance";
+let addExpenses = "Internet, Taxi, Food, Communal Apartment";
+let deposit = true;
+let mission = 1000000;
+let period  = 7;
+
+
+console.log(typeof money);
+console.log(typeof deposit);
+console.log(typeof income);
+console.log(addExpenses.length);
+console.log("Период равен ", period, " месяцев");
+console.log("Цель заработать", mission, " рублей");
+console.log(addExpenses.toLowerCase());
+
+let budgetDay = money / 30;
+console.log('budgetDay: ', budgetDay);
